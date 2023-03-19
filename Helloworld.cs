61c4a1e9b5e963f480a2fc702068ba4c56fe78ee
Mateusz Dargiel <remuluson2@gmail.com>
2023-03-19 11:52:58 +0100

    Etap 0

---
+++ Helloworld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD_Programowanie_Wspolbiezne
+{
+    public class Helloworld
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# MD-Programowanie-Wspolbiezne
+# Concurrent programming
+
+## Working Group
+
+| Name Surname (initials) | GUID                                     |
+| ----------------------- | ---------------------------------------- |
+| MD                      | `{1C04AA3F-3BE6-423C-AB50-31095DDEF1DD}` |
