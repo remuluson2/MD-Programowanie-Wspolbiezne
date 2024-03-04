51ebf33a843cc710ad25b69c5d9e5fa6331f4f4e
Mateusz Dargiel <229860@edu.p.lodz.pl>
2024-03-04 15:15:19 +0100

    implemented datalayer as IBallHolder which expands collection interface, added tests to datalayer

--- BallFormsWPFMD/MainWindow.xaml.cs
+++ BallFormsWPFMD/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BallFormsWPFMD
+namespace PresentationLayer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
---
+++ MSTest/DataLayerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+
+namespace MSTest
+{
+    [TestClass]
+    public class DataLayerTest
+    {
+        [TestMethod, TestCategory("DataLayer")]
+        public void TestAddAndRemove()
+        {
+            IBallHolder holder = new BallHolder();
+            IBall ball1 = new Ball();
+            holder.Add(ball1);
+            Assert.IsTrue(holder.Contains(ball1));
+            holder.Remove(ball1);
+            Assert.IsFalse(holder.Contains(ball1));
+        }
+        [TestMethod, TestCategory("DataLayer")]
+        public void TestCount()
+        {
+            IBallHolder holder = new BallHolder();
+            IBall ball1 = new Ball();
+            IBall ball2 = new Ball();
+
+            holder.Add(ball1);
+            holder.Add(ball2);
+            Assert.AreEqual(2, holder.Count);
+            Assert.AreNotEqual(0, holder.Count);
+
+            holder.Clear();
+            Assert.AreEqual(0, holder.Count);
+        }
+    }
+}
