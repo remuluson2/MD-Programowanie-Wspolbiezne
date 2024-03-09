6bfe3b7adb90f35adde31934e8640ced5b90e6b6
Mateusz Dargiel <remuluson2@gmail.com>
2024-03-09 11:08:11 +0100

    check ignores

--- BallFormsWPFMD/obj/Debug/net6.0-windows/BallFormsWPFMD.AssemblyInfo.cs
+++ BallFormsWPFMD/obj/Debug/net6.0-windows/BallFormsWPFMD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BallFormsWPFMD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7698e5de26910d53af61161038265039909aa448")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51ebf33a843cc710ad25b69c5d9e5fa6331f4f4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BallFormsWPFMD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BallFormsWPFMD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
