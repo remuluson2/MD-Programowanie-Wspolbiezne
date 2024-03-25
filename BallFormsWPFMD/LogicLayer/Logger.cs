28649ce1eca022c50367a8289ad7d601fe9e7915
Mateusz Dargiel <remuluson2@gmail.com>
2024-03-25 09:02:20 +0100

    created logger

--- BallFormsWPFMD/LogicLayer/Logger.cs
+++ BallFormsWPFMD/LogicLayer/Logger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,14 @@ namespace LogicLayer
 {
     public class Logger
     {
+        public async Task LogMessage(string message)
+        {
+            string logfilepath = "log.txt";
 
+            using (StreamWriter logWriter = new StreamWriter(logfilepath))
+            {
+                await logWriter.WriteLineAsync(message);
+            }
+        }
     }
 }
